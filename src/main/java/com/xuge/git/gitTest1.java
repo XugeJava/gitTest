fc2769e7b8686f848bb513a6ca2ff9d48ceee65f
xuge <xuge@qq.com>
2022-03-13 13:22:12 +0800

    my test2 git

--- src/main/java/com/xuge/git/gitTest1.java
+++ src/main/java/com/xuge/git/gitTest1.java
@@ -7,5 +7,6 @@ package com.xuge.git;
 public class gitTest1 {
   public static void main(String[] args) {
     System.out.println("my first git demo!!");
+    System.out.println("my second git demo!");
   }
 }
