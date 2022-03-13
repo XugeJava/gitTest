b987aaf90654096c7234c2b0c82f50f20cd47421
xuge <xuge@qq.com>
2022-03-13 13:15:10 +0800

    my test git

---
+++ src/main/java/com/xuge/git/gitTest1.java
@@ -0,0 +1,11 @@
+package com.xuge.git;
+
+/**
+ * author: yjx
+ * Date :2022/3/1313:10
+ **/
+public class gitTest1 {
+  public static void main(String[] args) {
+    System.out.println("my first git demo!!");
+  }
+}
